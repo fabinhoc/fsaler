2c1378060d76b70a23c99ddf531c833a0e237fdc
Fabio Cruz <fabinhocosta1202@gmail.com>
2024-04-16 20:19:51 -0300

    :sparkles: Add Sale model and migration

---
+++ app/Models/Sale.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sale extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'client_id',
+        'payment_type_id',
+        'payment_date',
+        'is_paid',
+        'total',
+        'total_paid',
+        'description',
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function paymentType(): BelongsTo
+    {
+        return $this->belongsTo(PaymentType::class);
+    }
+}
---
+++ database/migrations/2024_04_16_231007_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('payment_type_id')->nullable()->constrained()->nullOnDelete();
+            $table->date('payment_date')->nullable();
+            $table->boolean('is_paid')->nullable()->default(false);
+            $table->decimal('total')->default(0);
+            $table->decimal('total_paid')->nullable()->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
